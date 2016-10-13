a67550570d40f86de1a4583f491859a5516cd70d
Alan Smith <a.bax.smith@gmail.com>
2016-10-12 22:08:37 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//ROUTES GO HERE:
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
+});
+
+var server = app.listen(PORT, function() {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('App listening at http://%s:%s', host, port)
+});
